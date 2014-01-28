8ded9c33fa8b4728ca7eff30e4d0c52791aef5d9
unknown <Scott@NH-LAPTOP-225.wiggledomain.wiggle.co.uk>
2014-01-28 10:43:03 +0000

    Removing deleted files

--- DDDPPP.Chap19.EFExample/DDDPPP.Chap19.EFExample.Application/Infrastructure/EntityBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DDDPPP.Chap19.EFExample.Application.Infrastructure
-{
-    public abstract class EntityBase
-    {
-        public int Version { get; private set; }
-    }
-}
